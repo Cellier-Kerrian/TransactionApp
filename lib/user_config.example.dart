4f3ab835dea687d01ef5ba46179d2249961e3279
Cellier-Kerrian <kerrian.cellier@gmail.com>
2025-11-21 22:14:13 +0100

    Update

--- lib/user_config.example.dart
+++ lib/user_config.example.dart
@@ -3,12 +3,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class UserConfig {
   // ⚠️ Exemple uniquement, à copier en user_config.dart et à personnaliser
-
   static String USER_NAME = '';
-  static String GITHUB_TOKEN  = '';
+  static String GITHUB_TOKEN = '';
 
-  static String get CSV_PATH => "data/${USER_NAME}/user_transactions.csv";
-  static String get CONFIG_PATH => "data/${USER_NAME}/config.json";
+  static String get CSV_PATH => 'data/$USER_NAME/user_transactions.csv';
+  static String get CONFIG_PATH => 'data/$USER_NAME/config.json';
 
   static const String GITHUB_OWNER  = "owner_name";
   static const String GITHUB_REPO   = "repo_name";
@@ -16,8 +15,8 @@ class UserConfig {
 
   static const Color APP_SEED_COLOR = Colors.grey;
 
-  static String CODE_USERNAME = '0001';
   static String CODE_TOKEN = '0000';
+  static String CODE_USERNAME = '0001';
 
   static const String _kUserName = 'user_name';
   static const String _kToken = 'github_token';
@@ -28,15 +27,15 @@ class UserConfig {
     GITHUB_TOKEN = p.getString(_kToken) ?? '';
   }
 
-  static Future<void> setUserName(String name) async {
-    USER_NAME = name;
+  static Future<void> setGitHubToken(String v) async {
+    GITHUB_TOKEN = v;
     final p = await SharedPreferences.getInstance();
-    await p.setString(_kUserName, name);
+    await p.setString(_kToken, v);
   }
 
-  static Future<void> setGitHubToken(String token) async {
-    GITHUB_TOKEN = token;
+  static Future<void> setUserName(String v) async {
+    USER_NAME = v;
     final p = await SharedPreferences.getInstance();
-    await p.setString(_kToken, token);
+    await p.setString(_kUserName, v);
   }
 }
